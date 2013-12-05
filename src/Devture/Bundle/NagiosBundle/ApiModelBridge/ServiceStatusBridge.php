6ade1d252d8a44524688d9a9deedb71d450f223e
Slavi Pantaleev <s.pantaleev@gmail.com>
2013-12-05 16:53:24 +0200

    Do not export all service status directives
    It makes the API response 2x larger at the moment.

--- src/Devture/Bundle/NagiosBundle/ApiModelBridge/ServiceStatusBridge.php
+++ src/Devture/Bundle/NagiosBundle/ApiModelBridge/ServiceStatusBridge.php
@@ -6,7 +6,27 @@ use Devture\Bundle\NagiosBundle\Status\ServiceStatus;
 class ServiceStatusBridge {
 
 	public function export(ServiceStatus $entity) {
-		return $entity->getDirectives();
+		static $exportedDirectives = array(
+			'current_attempt',
+			'current_state',
+			'has_been_checked',
+			'last_check',
+			'last_hard_state',
+			'last_hard_state_change',
+			'last_state_change',
+			'max_attempts',
+			'next_check',
+			'performance_data',
+			'plugin_output',
+		);
+
+		$status = array();
+		foreach ($entity->getDirectives() as $directiveName => $directiveValue) {
+			if (in_array($directiveName, $exportedDirectives)) {
+				$status[$directiveName] = $directiveValue;
+			}
+		}
+		return $status;
 	}
 
 }
